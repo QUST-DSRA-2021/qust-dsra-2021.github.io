cd40928174acfa7c29ccbff022a69a1ada46e296
Your Name <you@example.com>
2022-05-23 15:11:27 +0000

    Site updated: 2022-05-23 15:11:24

--- 2022/03/29/pylesson-04/统计字符个数问题.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Python
-# -*- coding: utf-8 -*-
-"""
-@author: 陈 子坤 (GitHub@sandyzikun)
-@title: 统计字符个数问题
-@time: 2022-04-06 15:39:39
-"""
-st = input("请输入一个字符串:")
-print("该字符串中含有%d个字母p,含有%d个字母y" % (st.count("p"), st.count("y")))
--- 2022/03/29/pylesson-04/较大.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Python
-# -*- coding: utf-8 -*-
-"""
-@author: 陈 子坤 (GitHub@sandyzikun)
-@title: 较大
-@time: 2022-04-12 11:22:39 (乐正绫7th发售日快乐!!)
-"""
-if __name__ == "__main__": print("It\'s the Greater One: %d" % (lambda a, b: a if a >= b else b)(*eval(input("Input 2 Nums, Seperated by a Comma: "))))
